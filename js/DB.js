996b287ee51ef4eb2ad4973e553cd046fe164af4
elefantinhos2 <douglasramires01@gmail.com>
2023-03-15 04:20:14 -0300

    feat: preparando repositorio

---
+++ js/DB.js
@@ -0,0 +1,50 @@
+let DataBase = [
+    {
+        id: 1,
+        nome: 'Lightweight Jacket',
+        tipo: "Camiseta",
+        img: 'img/Men-Jacket-Front-Black__15466 1.png',
+        descricao: 'Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...',
+        preco: 'R$ 100.00'
+    },
+    {
+        id: 2,
+        nome: 'Black Hat',
+        tipo: "Acessório",
+        img: 'img/Men-Jacket-Front-Black__1.png',
+        descricao: 'O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...',
+        preco: 'R$ 100.00'
+    },
+    {
+        id: 3,
+        nome: 'Mask',
+        tipo: "Acessório",
+        img: 'img/Men-Jacket-Front-Black__2.png',
+        descricao: 'Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...',
+        preco: 'R$ 100.00'
+    },
+    {
+        id: 4,
+        nome: 'T-Shirt',
+        tipo: "Camiseta",
+        img: 'img/Men-Jacket-Front-Black__3.png',
+        descricao: 'Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...',
+        preco: 'R$ 100.00'
+    },
+    {
+        id: 5,
+        nome: 'Short-Sleeve T-Shirt',
+        tipo: "Camiseta",
+        img: 'img/Men-Jacket-Front-Black__4.png',
+        descricao: 'Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais fino...',
+        preco: 'R$ 100.00'
+    },
+    {
+        id: 6,
+        nome: 'Champion Packable Jacket',
+        tipo: "Camiseta",
+        img: 'img/Men-Jacket-Front-Black__5.png',
+        descricao: 'Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...',
+        preco: 'R$ 100.00'
+    },
+]
