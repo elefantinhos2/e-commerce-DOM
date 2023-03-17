08c22de0d784966cd0fe75351575e400869c5dd1
elefantinhos2 <douglasramires01@gmail.com>
2023-03-16 22:34:46 -0300

    feat: create news functions

--- models/vitrine.js
+++ models/vitrine.js
@@ -38,4 +38,59 @@ class Vitrine  {
 
         return li
     }
+
+    listaProdutosCarrinho() {
+        const corpoLista = document.querySelector('.containerListaProdutos ul')
+    
+        this._querySelector.innerHTML = ' '
+        const teste = this._ListaProduto.forEach((produto) => {
+            const card = this.cardProdutoCarrinho(produto)
+            console.log(card)
+            this._querySelector.appendChild(card)
+            return card
+        })
+        return teste
+
+    }
+
+    cardProdutoCarrinho = ({id, nome, tipo, img, descricao, preco}) => {
+        const li      = document.createElement('li')
+
+        li.innerHTML = `
+            <figure>
+                <img src="${img}" alt="${nome}">
+            </figure>
+            <div>
+                <h3>${nome}</h3>
+                <p>${preco}</p>
+                <button class="buttonRemove" id=${id}>Remover Item</button>
+            </div>
+        `
+        return li
+    }
+
+    ListaCarrinhoVazio() {
+
+        this._querySelector.innerHTML = ' '
+
+       
+        const card = this.cardCarrinhoVazio()
+
+        this._querySelector.appendChild(card)
+        return card
+
+
+    }
+
+    cardCarrinhoVazio() {
+        
+        const  div = document.createElement('div')
+
+        div.innerHTML = `
+            <h5>Carrinho Vazio</h5>
+            <h6>Adicione Items</h6>
+        `
+
+        return div
+    }
 }
